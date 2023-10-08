e66c2a16cb71dd3e43e8950f80b7353a7d2eb174
pmj1459 <76741702+minju1459@users.noreply.github.com>
2023-10-09 03:21:47 +0900

    [Level.0] 나머지 구하기

---
+++ 나머지 구하기.py
@@ -0,0 +1,8 @@
+def solution(num1, num2):
+    solution = lambda num1, num2 : num1 % num2
+    answer = -1
+    answer = num1 % num2
+    return answer
+
+## 람다식
+solution = lambda num1, num2 : num1 % num2
