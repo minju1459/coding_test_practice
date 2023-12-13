5530ee9a786001e914121e4d471111877d87e497
Minju Park <76741702+minju1459@users.noreply.github.com>
2023-12-13 21:46:48 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,15 @@
+n = int(input())
+number = list(map(int, input().split()))
+result = 0
+
+for num in number:
+    cnt = 0
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                cnt += 1
+        
+        if cnt == 0:
+            result += 1
+    
+print(result)
