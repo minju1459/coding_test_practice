5bcc7c9dee2c5df2e50586fca2bfc51aaea4e312
pmj1459 <76741702+minju1459@users.noreply.github.com>
2024-04-22 19:03:12 +0900

    그리디 - 숫자 카드 게임 min()

--- 이코테/큰 수의 법칙.py
+++ 이코테/큰 수의 법칙.py
@@ -5,17 +5,11 @@ data.sort()
 first = data[n-1]
 second = data[n-2]
 
+count = int(m / (k+1)) * k
+count += m % (k+1)
+
 result = 0
+result += (count)*first
+result += (m-count) * second
 
-while True:
-    for i in range(k):
-        if m==0:
-            break
-        result += first 
-        m -=1
-    if m == 0:
-        break
-    result += second
-    m -=1
-
-    print(result)
+print(result)
